d2e545ae6436afaf02e888d9aabc11051bc58c4b
thalesrmacedo <165821269+thalesrmacedo@users.noreply.github.com>
2025-03-20 15:46:39 -0300

    Create middeware_autenticacao.js

---
+++ middeware_autenticacao.js
@@ -0,0 +1,19 @@
+//COLOCAR NA LINHA 18 (ANTES DE ROTA DE LOGIN)
+
+
+
+
+
+// Middleware de autenticação
+function authenticateToken(req, res, next) {
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+
+    if (!token) return res.sendStatus(401);
+
+    jwt.verify(token, SECRET_KEY, (err, user) => {
+        if (err) return res.sendStatus(403);
+        req.user = user;
+        next();
+    });
+}
