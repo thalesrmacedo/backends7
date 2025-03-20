ef4fd14344d3edf90985a2d5f7fb799285881952
thalesrmacedo <165821269+thalesrmacedo@users.noreply.github.com>
2025-03-20 15:58:23 -0300

    Delete rota_protegida.js

--- rota_protegida.js
+++
@@ -1,7 +0,0 @@
-//COLOCAR APÓS ROTA DE LOGIN
-
-
-// Rota protegida - acesso para todos os usuários autenticados
-app.get('/tasks', authenticateToken, (req, res) => {
-    res.json({ tasks: ['Tarefa 1', 'Tarefa 2'] });
-});
