f12d244cc8c7b5cc83895bed72f6f6a4653e9d20
thalesrmacedo <165821269+thalesrmacedo@users.noreply.github.com>
2025-03-21 07:44:40 -0300

    Update index.js

--- index.js
+++ index.js
@@ -3,20 +3,39 @@ const jwt = require('jsonwebtoken');
 const app = express();
 app.use(express.json());
 
-const SECRET_KEY = 'my_secret_key';
+const SECRET_KEY = 'my_secret_key'; // Chave para assinar tokens
 
-// Usuários fictícios
 const users = [
     { id: 1, username: 'user1', password: 'password1', role: 'user' },
     { id: 2, username: 'admin', password: 'adminpass', role: 'admin' }
 ];
 
-// Função para gerar token JWT
 function generateToken(user) {
     return jwt.sign({ id: user.id, role: user.role }, SECRET_KEY, { expiresIn: '1h' });
 }
 
-// Rota de login
+function authenticateToken(req, res, next) {
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+
+    if (!token) return res.sendStatus(401);
+
+    jwt.verify(token, SECRET_KEY, (err, user) => {
+        if (err) return res.sendStatus(403);
+        req.user = user;
+        next();
+    });
+}
+
+function authorizeRole(role) {
+    return (req, res, next) => {
+        if (req.user.role !== role) {
+            return res.sendStatus(403);
+        }
+        next();
+    };
+}
+
 app.post('/login', (req, res) => {
     const { username, password } = req.body;
     const user = users.find(u => u.username === username && u.password === password);
@@ -25,11 +44,18 @@ app.post('/login', (req, res) => {
         const token = generateToken(user);
         res.json({ token });
     } else {
-        res.sendStatus(401); // Usuário ou senha inválidos
+        res.sendStatus(401);
     }
-}); 
+});
+
+app.get('/tasks', authenticateToken, (req, res) => {
+    res.json({ tasks: ['Tarefa 1', 'Tarefa 2'] });
+});
+
+app.post('/admin/users', authenticateToken, authorizeRole('admin'), (req, res) => {
+    res.send('Usuário criado com sucesso.');
+});
 
 app.listen(3000, () => {
     console.log('Servidor rodando na porta 3000');
 });
-
